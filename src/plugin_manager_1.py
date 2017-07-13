7bb9f5585bd2e90832e091e942ac9548c59b5df5
Sijis Aviles <saviles@gogoair.com>
2017-07-13 11:14:37 -0500

    feat: Add pluginbase example

---
+++ src/plugin_manager_1.py
@@ -0,0 +1,21 @@
+"""This is an example using PluginBase
+
+Install the necessary package with
+$ pip install pluginbase
+"""
+from pluginbase import PluginBase
+
+
+plugin_base = PluginBase(package='foremast.xplugins')
+
+plugin_source = plugin_base.make_plugin_source(
+    searchpath=['./foremast_1_aws/plugins/aws'])
+
+print('List all plugins')
+for plugin in plugin_source.list_plugins():
+    print(plugin)
+
+print('Call plugin directly.')
+my_plugin = plugin_source.load_plugin('dns')
+dns = my_plugin.Dns()
+print(dns.create())
